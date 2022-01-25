bce8e370bc67146598b1af21048b3116d494b663
dw3nt <rick@dualwieldent.com>
2022-01-25 12:02:25 -0700

    base SDK object handling token auth

--- src/ForeUp.php
+++ src/ForeUp.php
@@ -2,9 +2,66 @@
 
 namespace Dw3nt\ForeUpSdk;
 
+use \GuzzleHttp\Client;
+
 class ForeUp
 {
-    public static function hello() {
-        return "Hello!";
+    private $token;
+    private $httpClient;
+    private $baseUri = "https://api.foreupsoftware.com/api_rest/index.php/";
+
+    public function __construct()
+    {  
+        $data = $this->createNewToken([
+            'email' => $this->getSdkEnv("FORE_UP_USER"),
+            'password' => $this->getSdkEnv("FORE_UP_PASSWORD")
+        ]);
+
+        $this->token = $data['data']['id'];
+    }
+
+    public function getHttpClient()
+    {
+        if (!$this->httpClient) {
+            $this->httpClient = new Client([
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'x-authorization' => 'Bearer ' . $this->token
+                ]
+            ]);
+        }
+
+        return $this->httpClient;
+    }
+
+    public function createNewToken($bodyData)
+    {
+        $client = new Client();
+        $url = $this->baseUri . "tokens";
+        $response = $client->request('POST', $url, [
+            'form_params' => $bodyData
+        ]);
+        return json_decode($response->getBody(), true);
+    }
+
+    public function request($method, $uri, $params = []) 
+    {
+        $client = $this->getHttpClient();
+        return $client->request($method, $uri, $params);
+    }
+
+    public function getSdkEnv($key = "") 
+    {
+        $env = file_get_contents(__DIR__ . "/../.env");
+        $data = explode("\n", $env);
+        $keys = array_map(fn ($line) => explode('=', $line)[0], $data);
+        $values = array_map(fn ($line) => explode('=', $line)[1], $data);
+        $data = array_combine($keys, $values);
+
+        if (array_key_exists($key, $data)) {
+            return $data[$key];
+        }
+
+        return "";
     }
 }
